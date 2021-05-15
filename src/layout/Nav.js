09f255c035720a6081e06fd212dc3267615ffcdb
KhaledShehataA <khaled.ah.shehata@gmail.com>
2021-05-15 12:21:01 +0200

    import router-dom

--- src/layout/Nav.js
+++ src/layout/Nav.js
@@ -1,15 +1,23 @@
+import { NavLink } from 'react-router-dom';
+
 const Nav = () => {
 	return (
-		<nav>
-			<ul>
+		<nav className='bg-purple-600 text-white p-4 shadow'>
+			<ul className='flex space-x-6 justify-end'>
 				<li>
-					<a href='#'>Home</a>
+					<NavLink to='/' activeClassName='border border-white'>
+						Home
+					</NavLink>
 				</li>
 				<li>
-					<a href='#'>About</a>
+					<NavLink to='/about' activeClassName='border border-white'>
+						About
+					</NavLink>
 				</li>
 				<li>
-					<a href='#'>Contact</a>
+					<NavLink to='/contact' activeClassName=''>
+						Contact
+					</NavLink>
 				</li>
 			</ul>
 		</nav>
--- src/layout/index.js
+++ src/layout/index.js
@@ -1,10 +1,6 @@
 import { Nav } from './Nav';
 
 const Layout = (props) => {
-	return (
-		<div className='bg-purple-500 border border-black m-5'>
-			{props.children}
-		</div>
-	);
+	return <div>{props.children}</div>;
 };
 export { Layout, Nav };
